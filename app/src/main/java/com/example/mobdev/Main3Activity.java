c96aac6cb46f907a8572e276f9e21d0dcf59afde
ahmadafrasiyab <afrasiyab.ahmad@hotmail.com>
2020-05-16 16:55:34 -0700

    added data transfer from activity 3 to profile fragment

--- app/src/main/java/com/example/mobdev/Main3Activity.java
+++ app/src/main/java/com/example/mobdev/Main3Activity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import com.google.android.material.tabs.TabLayout;
 
@@ -12,21 +13,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
-import android.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class Main3Activity<viewPager> extends AppCompatActivity {
 
-    private String name = "";
-    private String email = "";
-    private String age = "";
-    private String description = "";
-    private String occupation = "";
-    private String dateOfBirth = "";
+public class Main3Activity<viewPager> extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,48 +38,23 @@ public class Main3Activity<viewPager> extends AppCompatActivity {
         tabs.addTab(tabs.newTab().setText("Matches"));
         tabs.addTab(tabs.newTab().setText("Settings"));
         tabs.setupWithViewPager(viewPager);
-
-        Intent intent = getIntent();
-        Bundle b = intent.getExtras();
-
-
-        if (b != null) {
-            if (b.containsKey(Constants.KEY_NAME)) {
-                name = b.getString(Constants.KEY_NAME);
-            }
-
-            if (b.containsKey(Constants.KEY_EMAIL)) {
-                email = b.getString(Constants.KEY_EMAIL);
-            }
-
-            if (b.containsKey(Constants.KEY_AGE)) {
-                age = b.getString(Constants.KEY_AGE);
-            }
-
-            if (b.containsKey(Constants.KEY_DESCRIPTION)) {
-                description = b.getString(Constants.KEY_DESCRIPTION);
-            }
-
-            if (b.containsKey(Constants.KEY_OCCUPATION)) {
-                occupation = b.getString(Constants.KEY_OCCUPATION);
-            }
-
-            if (b.containsKey(Constants.KEY_DATE_OF_BIRTH)) {
-                dateOfBirth = b.getString(Constants.KEY_DATE_OF_BIRTH);
-            }
-
-
-        }
     }
 
-        public void goToFirstActivity (View view){
+
+
+
+    public void goToFirstActivity (View view){
             finish();
         }
 
         // Add Fragments to Tabs
         private void setupViewPager (ViewPager viewPager){
             Adapter adapter = new Adapter(getSupportFragmentManager());
-            adapter.addFragment(new ProfileFragment(), "Profile");
+            ProfileFragment profile = new ProfileFragment();
+            adapter.addFragment(profile, "Profile");
+            Intent intent = getIntent();
+            Bundle b = intent.getExtras();
+            profile.setArguments(b);
             adapter.addFragment(new MatchesFragment(), "Matches");
             adapter.addFragment(new SettingsFragment(), "Settings");
             viewPager.setAdapter(adapter);
@@ -98,10 +68,12 @@ public class Main3Activity<viewPager> extends AppCompatActivity {
                 super(manager);
             }
 
+            @NonNull
             @Override
             public Fragment getItem(int position) {
                 return mFragmentList.get(position);
             }
+
 
             @Override
             public int getCount() {
--- app/src/main/java/com/example/mobdev/ProfileFragment.java
+++ app/src/main/java/com/example/mobdev/ProfileFragment.java
@@ -13,11 +13,69 @@ import android.widget.TextView;
  */
 public class ProfileFragment extends Fragment {
     private String name = "";
+    private String email = "";
+    private String age = "";
+    private String description = "";
+    private String occupation = "";
+    private String dateOfBirth = "";
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.profile_fragment, null);
 
-        return inflater.inflate(R.layout.activity_main2, null);
+        TextView textViewName = root.findViewById(R.id.textViewName);
+        TextView textViewAge = root.findViewById(R.id.textViewAge);
+        TextView textViewEmail = root.findViewById(R.id.textViewEmail);
+        TextView textViewDescription = root.findViewById(R.id.textViewDescription);
+        TextView textViewOccupation = root.findViewById(R.id.textViewOccupation);
+        TextView textViewDateOfBirth = root.findViewById(R.id.textViewDateOfBirth);
+
+
+        Bundle b = getArguments();
+
+        if (b != null) {
+            if (b.containsKey(Constants.KEY_NAME)) {
+                name = b.getString(Constants.KEY_NAME);
+            }
+
+            if (b.containsKey(Constants.KEY_EMAIL)) {
+                email = b.getString(Constants.KEY_EMAIL);
+            }
+
+            if (b.containsKey(Constants.KEY_AGE)) {
+                age = b.getString(Constants.KEY_AGE);
+            }
+
+            if (b.containsKey(Constants.KEY_DESCRIPTION)) {
+                description = b.getString(Constants.KEY_DESCRIPTION);
+            }
+
+            if (b.containsKey(Constants.KEY_OCCUPATION)) {
+                occupation = b.getString(Constants.KEY_OCCUPATION);
+            }
+
+            if (b.containsKey(Constants.KEY_DATE_OF_BIRTH)) {
+                dateOfBirth = b.getString(Constants.KEY_DATE_OF_BIRTH);
+            }
+            name = "Name: " + name;
+            age = "Age: " + age;
+            email = "Email: " + email;
+            description = "Description: " + description;
+            occupation = "Occupation: " + occupation;
+            dateOfBirth = "Date Of Birth: " + dateOfBirth;
+
+            textViewName.setText(name);
+            textViewAge.setText(age);
+            textViewEmail.setText(email);
+            textViewDescription.setText(description);
+            textViewDateOfBirth.setText(dateOfBirth);
+            textViewOccupation.setText(occupation);
+
+
+        }
+        return root;
     }
 }
+
